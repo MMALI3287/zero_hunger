0071f87d886d7bf66e6fc0edb6db1f93d09b1d81
Mahtab12381 <mahtab.sani12381@gmail.com>
2023-03-17 05:40:02 +0600

    login/Registration

--- zero_hunger/Controllers/HomeController.cs
+++ zero_hunger/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using zero_hunger.EF;
+using zero_hunger.Models;
 
 namespace zero_hunger.Controllers
 {
@@ -26,5 +28,91 @@ namespace zero_hunger.Controllers
 
             return View();
         }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login model)
+        {
+            var db = new zero_hungerEntities2();
+            var extUser=(from u in db.Registrations where u.username==model.username && u.password==model.password select u).SingleOrDefault();
+            if (extUser != null)
+            {
+                if (extUser.user_type.Equals("Admin"))
+                {
+                    return RedirectToAction("Index","Admin");
+                }
+                if (extUser.user_type.Equals("Employees"))
+                {
+                    return RedirectToAction("Index","Employees");
+                }
+                if (extUser.user_type.Equals("Restaurents"))
+                {
+                    return RedirectToAction("Index","Restaurents");
+                }
+            }
+            else
+            {
+                ViewBag.msg = "Logn Failed";
+            }
+            return View(); 
+        }
+
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View();  
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(RegistrationClass model)
+        {
+            var db = new zero_hungerEntities2();
+            var user = new Registration()
+            {
+                username = model.username, password = model.password, user_type = model.user_type
+            };
+            db.Registrations.Add(user);
+            db.SaveChanges();
+            var rid=(from r in db.Registrations where r.username==model.username select r).SingleOrDefault();
+            if (model.user_type.Equals("Employees"))
+            {
+                var employee = new Employee()
+                {
+                    name= model.name,phone=model.phone,email=model.email,Rid=rid.id
+                };
+                db.Employees.Add(employee); 
+            }
+            if (model.user_type.Equals("Restaurents"))
+            {
+                var restaurents = new Restaurant()
+                {
+                    supplier_name = model.name,
+                    contact_number = model.phone,
+                    name="-",location="-",
+                    Rid = rid.id
+                };
+                db.Restaurants.Add(restaurents);
+            }
+            if (model.user_type.Equals("Admin"))
+            {
+                var admin = new Admin()
+                {
+                    name = model.name,
+                    phone = model.phone,
+                  email=model.email,
+                    Rid = rid.id
+                };
+                db.Admins.Add(admin);
+            }
+            db.SaveChanges();
+            ViewBag.msg = "Registration Successfull";
+            return View();
+        }
+
     }
 }
---
+++ zero_hunger/Models/RegistrationClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zero_hunger.Models
+{
+    public class RegistrationClass
+    {
+        public string user_type { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+        public string name { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+
+    }
+}
